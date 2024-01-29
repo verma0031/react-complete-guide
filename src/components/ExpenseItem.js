6af60c251d8fe2314348ad8b5d2571aac0dec32a
verma0031 <peeyushverma888@gmial.com>
2024-01-29 23:36:48 +0530

    Made ExpenseItem component and added some expenses

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function ExpenseItem(){
+    return (
+        <>
+        <header>
+            <h1>Expense Items !!</h1>
+        </header>
+
+        <h2>Food Rs 10</h2>
+        <h2>Petrol Rs 100</h2>
+        <h2>Movie Rs 200</h2>
+        
+        </>
+    )
+}
+
+export default ExpenseItem;
