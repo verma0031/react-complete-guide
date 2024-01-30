f969f4f27125a897824ca7fbb1ad8430fcf22189
verma0031 <peeyushverma888@gmial.com>
2024-01-30 19:10:33 +0530

    Organized code into file structure also used arrow functions

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './ExpenseDate.css'
 
 function expenseDate(props) {
   const month = props.date.toLocaleString("en-US", { month: "long" });
--- src/components/ExpenseDetails.js
+++ src/components/ExpenseDetails.js
@@ -3,10 +3,12 @@ import "./ExpenseItem.css";
 
 function ExpenseDetails(props) {
   return (
-    <div className="expense-item__description">
-      <div>{props.title}</div>
-      <div>{props.location}</div>
-      <div className="expense-item__price">{props.amount}</div>
+    <div>
+      <div className="expense-item__description">
+        {props.title}
+        <h2>{props.location}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
     </div>
   );
 }
